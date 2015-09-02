2953a501e5a2038facfa1af14a50ad3fecd9e1a8
pylipp <philipp.metzner@tum.de>
2015-09-02 20:55:14 +0200

    Some additions.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #/usr/bin/zsh 
 
-DOTFLS="~/Documents/dotfiles/"
+cd ~
+git clone git@github.com:pylipp/dotfiles.git 
+
+mv -r dotfiles .files
+DOTFLS="~/.files/"
 
 cd ~
 ln -s "$DOTFLS".vimrc .vimrc
 ln -s "$DOTFLS".vim .vim
 ln -s "$DOTFLS".ycm_conf_extra.py .ycm_conf_extra.py
+ln -s "$DOTFLS".gitconfig .gitconfig
+ln -s "$DOTFLS".gitignore_global .gitignore_global
 
 mkdir -p ~/.config/git-cola/
 ln "$DOTFLS".git-cola-settings ~/.config/git-cola/settings
