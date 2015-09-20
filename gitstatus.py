ad1ea7370ed79b7f9f51b0d7db4aa18bc3676856
pylipp <philipp.metzner@tum.de>
2015-09-20 15:24:42 +0200

    Some changes. Specifying repo via command line argument possible. Repos are stored in name-path-dictionary.

--- gitstatus.py
+++ gitstatus.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3 
 
 from subprocess import call
-import os
+import os 
+import sys
 
-repos = ["~/Documents/dotfiles",
-        "~/Documents/thesis/latex"
-        ] 
+repos = {}
+repos["dotfiles"] = "~/Dokumente/dotfiles"
+repos["mscthesis"] = "~/Dokumente/Documents/master project/thesis/mscthesis"
+repos["pythir"] = "~/Dropbox/code/pythir"
+repos["financeager"] = "~/Dropbox/code/financeager"
 
-for repo in repos:
-    path = os.path.expanduser(repo)
-    print(path)
+def main(repolist):
+    # Display status of repos specified as command line arguments.
+    if len(repolist):
+        seq = repolist 
+    # Display status of all repos in the repos-dictionary.
+    else:
+        seq = repos.keys()
+    for name in seq:
+        printGitStatus(name)
+
+def printGitStatus(name):
+    path = repos.get(name)
+    if path is None:
+        print("Git repo '{name}' not specified.".format(name=name))
+        return
+    path = os.path.expanduser(path)
+    print(name.upper())
     os.chdir(path)
     call(["git", "status", "-bs"])
     print()
+
+main(sys.argv[1:])
