4ac0b343c3a69f665241bdc19c1acdeeb7370d17
pylipp <philipp.metzner@tum.de>
2016-06-16 13:56:15 +0200

    Add bat file for link creation on windows.

---
+++ setup.bat
@@ -0,0 +1,5 @@
+mklink /H %USERPROFILE%\_vimrc %USERPROFILE%\Documents\.files\.vimrc
+mklink /H %USERPROFILE%\.gitignore_global %USERPROFILE%\Documents\.files\.gitignore_global
+mklink /H %USERPROFILE%\.gitconfig %USERPROFILE%\Documents\.files\.gitconfig
+mklink /H %USERPROFILE%\.gvimrc %USERPROFILE%\Documents\.files\.gvimrc
+mklink /J %USERPROFILE%\.vim %USERPROFILE%\Documents\.files\.vim
