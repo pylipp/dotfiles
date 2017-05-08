baa50c8bed4e1a6a191f56a7eba1e63535dda2bb
pylipp <beth.aleph@yahoo.de>
2017-05-08 12:35:17 +0200

    Source vim plugins last in vimrc to have leader key defined.

--- generate_vimrc.sh
+++ generate_vimrc.sh
@@ -2,13 +2,13 @@
 echo "set nocompatible" > vimrc
 echo >> vimrc
 
+for file in settings mappings functions; do
+    echo "source ~/.vim/startup/$file.vim" >> vimrc 
+done
+
 echo
 read -n 1 -p "Install vim plugins? y/n " reply
 echo
 if [[ "$reply" == 'y' ]]; then 
     echo "source ~/.vim/startup/plugins.vim" >> vimrc 
 fi 
-
-for file in settings mappings functions; do
-    echo "source ~/.vim/startup/$file.vim" >> vimrc 
-done
