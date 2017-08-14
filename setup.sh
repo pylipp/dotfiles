924822a4cbdca413c9202f3910dedba179cc9412
pylipp <philipp.metzner@tum.de>
2017-08-15 01:52:16 +0200

    Install hack font at setup.

--- setup.sh
+++ setup.sh
@@ -67,7 +67,7 @@ install_core_utils() {
     ~/.fzf/install --all
 
     # sudo apt-get install -y thefuck
-    install_packages exuberant-ctags tmux cmake tig zathura htop \
+    install_packages exuberant-ctags tmux cmake tig zathura htop fonts-hack-ttf \
         network-manager usbmount libxml2-dev libxslt-dev pulseaudio libasound2-dev \
         libxcb-composite0-dev python-dev curl doxygen graphviz
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
