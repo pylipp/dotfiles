f5a30cbbef1ba80eaba1695585c73ff078963a03
pylipp <philipp.metzner@tum.de>
2017-08-29 15:01:28 +0200

    Add mailcap config.

--- setup.sh
+++ setup.sh
@@ -287,7 +287,7 @@ setup_links() {
     cd $HOME
     for rcfile in ycm_extra_conf.py gitconfig bashrc zshrc tmux.conf i3 \
         Xresources xinitrc dir_colors latexmkrc pylintrc tigrc direnvrc \
-        pythonrc
+        pythonrc mailcap
     do
         link_name="."$rcfile
         link_path=$HOME/$link_name
