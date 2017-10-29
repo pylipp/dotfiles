fba13da775c2b4dfbd5143544a68ef9e6115a40f
pylipp <philipp.metzner@tum.de>
2017-10-29 21:46:56 +0100

    Add borg backup script incl shell reminder.

--- backup.bash
+++ backup.bash
@@ -1,35 +1,86 @@
 #!/bin/bash 
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: backup.bash DESTINATION"
-    exit
+# requires borg: sudo apt-get install borgbackup
+
+# Before running this script, set up the backup storage dir once:
+#   cd /mnt/ubuntu
+#   sudo mkdir backup
+#   sudo chown $USER:$USER backup
+#   borg init backup
+
+MOUNTPOINT=/mnt/ubuntu
+[[ ! -e $MOUNTPOINT ]] && sudo mkdir $MOUNTPOINT
+BACKUPDIR=$MOUNTPOINT/backup
+
+if [[ ! -d $BACKUPDIR ]]; then
+    PARTITION=/dev/sdb6
+    if [[ -e $PARTITION ]]; then
+        sudo mount $PARTITION $MOUNTPOINT
+    else
+        echo "Could not mount $PARTITION to $MOUNTPOINT!"
+        exit 1
+    fi
 fi
 
 cd ~
 
-#udiskctl mount -b /dev/sdb2
-destination=$1/ubuntu16-04-backup
+read -sp "Enter passphrase for \"$BACKUPDIR\": " BORG_PASSPHRASE
+echo ""
+export BORG_PASSPHRASE
 
-# does not matter whether destination has a trailing slash or not
-echo "Backing up to $destination..."
+# backing up my
+#   - data
+#   - configs
+#   - mail/browser stuff
+#   - Python virtualenvs
+#   - ZSH history
+# excluding binary and build files created by latex or python
 
-# trailing slash to copy subfolders of HOME
-# don't use pattern with starting slash
-sudo rsync -azv --progress --delete --delete-excluded \
-    --exclude $USER/.adobe \
-    --exclude .cache \
-    --exclude .cargo \
-    --exclude .compiz \
-    --exclude .dbus \
-    --exclude .dropbox* \
-    --exclude .gnome2* \
-    --exclude .gradle \
-    --exclude .java \
-    --exclude .macromedia \
-    --exclude .m2 \
-    --exclude .oracle_jre_usage \
-    --exclude .zcompdump* \
-    --exclude "VirtualBox VMs" \
-    --exclude build \
-    --exclude __pycache__ \
-    $HOME/ $destination
+CONFIGDIR=~/.config/borg
+LOGDIR=$CONFIGDIR/logs
+mkdir -p $LOGDIR
+
+BACKUPDATE=$(date +%Y%m%d)
+LOGFILE=$LOGDIR/$BACKUPDATE.log
+BORGARCHIVE=$BACKUPDIR::$BACKUPDATE
+
+borg create --verbose --stats --compression lz4 $BORGARCHIVE \
+    --exclude "sh:**/build" \
+    --exclude "*__pycache__*" \
+    --exclude "*.pyc" \
+    \
+    code \
+    Documents \
+    Pictures \
+    \
+    .config \
+    .files/local_shrc \
+    .files/global_gituser \
+    .screenlayout \
+    .vimrc \
+    \
+    .mozilla/firefox \
+    .cache/thunderbird \
+    .local/share/qutebrowser \
+    .thunderbird \
+    \
+    .virtualenvs \
+    \
+    .zsh_history \
+    >> $LOGFILE 2>&1
+
+# clean up old backups
+borg prune --verbose --stats \
+    --keep-within=2m \
+    --keep-monthly=2 \
+    --keep-yearly=2 \
+    $BACKUPDIR \
+    >> $LOGFILE 2>&1
+
+unset BORG_PASSPHRASE
+
+# store current UNIX time stamp to remind when next backup is due
+BACKUP_DATES_FILE=$CONFIGDIR/backup_dates
+date +%s >> $BACKUP_DATES_FILE
+
+sudo umount $MOUNTPOINT
