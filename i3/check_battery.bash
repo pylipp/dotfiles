cc3bba6b53db681cc541972b90a967147a5e9be1
pylipp <philipp.metzner@tum.de>
2017-12-11 17:41:28 +0100

    Improve check battery script.

--- i3/check_battery.bash
+++ i3/check_battery.bash
@@ -1,13 +1,22 @@
 #! /bin/bash
 
-# Utility script to issue a warning at low battery level (<15%).
+# Utility script to issue a warning at low battery level (<15%) and eventually
+# shutdown the machine at a level <10%.
 
 while true; do
     capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+    status_=$(cat /sys/class/power_supply/BAT0/status)
 
-    # display notification for 14 seconds and sleep for 15 to avoid overlap
-    if [[ $capacity -lt 15 ]]; then
-        notify-send -t 14000 -u critical WARNING "Remaining battery level: $capacity% !!"
+    if [[ $status_ = "Discharging" ]]; then
+        # display notification for 14 seconds and sleep for 15 to avoid overlap
+        if [[ $capacity -lt 15 ]]; then
+            notify-send -t 14000 -u critical WARNING "Remaining battery level: $capacity% !!"
+        fi
+        if [[ $capacity -lt 10 ]]; then
+            notify-send -u critical WARNING "Low battery, powering off..."
+            sleep 3
+            systemctl poweroff
+        fi
     fi
 
     sleep 15
