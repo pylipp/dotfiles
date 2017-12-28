85f8470e62fbcee5227fede6b68188e367a27404
pylipp <philipp.metzner@tum.de>
2017-12-28 06:23:20 +0100

    Stop core_temperature.py script if another process already running.

--- i3/core_temperature.py
+++ i3/core_temperature.py
@@ -2,9 +2,16 @@
 
 import psutil
 import time
+import os.path
+import sys
+
+filepath = "/tmp/core_temperature"
+if os.path.exists(filepath):
+    raise SystemExit("{} exists, assuming that another process is running. "
+               "Aborting.".format(filepath))
 
 while True:
-    with open("/tmp/core_temperature", "w") as f:
+    with open(filepath, "w") as f:
         temperature = psutil.sensors_temperatures()['coretemp'][0].current
         print(int(temperature * 1000), file=f)
     time.sleep(0.5)
