f17d4be2950ee26057d70a2bcce3b21c9b921989
pylipp <philipp.metzner@tum.de>
2019-06-08 15:41:13 +0200

    Install i3lock from debian repos
    Not sure why I wanted to build it from source. Currently using version
    2.8 from the repos on my private machine

--- setup/debian/setup_i3.bash
+++ setup/debian/setup_i3.bash
@@ -18,7 +18,7 @@ main() {
         echo_install_info i3
 
         install_packages git build-essential
-        install_packages i3 xautolock xorg xinit feh scrot imagemagick arandr
+        install_packages i3 i3lock xautolock xorg xinit feh scrot imagemagick arandr
 
         mkcswdir
 
@@ -30,17 +30,6 @@ main() {
         git checkout v1.2.0
         make
         sudo make install
-
-        cd ..
-        install_packages pkg-config libxcb1 libpam-dev libcairo-dev \
-            libxcb-xinerama0-dev libev-dev libx11-dev libx11-xcb-dev libxkbcommon0 \
-            libxkbcommon-x11-0 libxcb-dpms0-dev libxcb-image0-dev libxcb-util0-dev \
-            libxcb-xkb-dev libxkbcommon-x11-dev libxkbcommon-dev dbus-x11 \
-            libxcb-randr0 libxcb-randr0-dev
-        git clone https://github.com/i3/i3lock
-        cd i3lock
-        git checkout 2.10  # does not require autotools
-        sudo make install 
     elif [[ $method = "local" ]]; then
         echo_warn "Sorry, local i3 installation is not possible."
     else
