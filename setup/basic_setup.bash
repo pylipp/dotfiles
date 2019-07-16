105ebc328601c9d9c9fcca2a8a186f4b07dbfab1
pylipp <philipp.metzner@tum.de>
2019-07-16 22:36:03 +0200

    Condense setup into single script
    - add sudo logic for installing packages

--- setup/basic_setup.bash
+++ setup/basic_setup.bash
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# Don't run this inside tmux or plugin installation will fail
+if [[ ! -z $TMUX ]]; then
+    # Don't run inside tmux or plugin installation will fail
+    echo "Please quit tmux!" >&2
+    exit 1
+fi
+
+# Check for sudo rights of current user
+if groups $USER | grep -q sudo; then
+    # Assume Debian-based distro
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get install wget git zsh gcc g++ make xclip vim python3-venv
+fi
 
 set -e
 
-if [[ ! -z $TMUX ]]; then
-    echo Please quit tmux!
-    exit
-fi
-
-source $(dirname "$0")/utils.bash
-
-# assume that vim is already installed
-install_packages wget git zsh gcc g++ make xclip
-
 cd
 
-ln -s .files/vimrc .vimrc
-ln -s .files/vim .vim
+ln -s ~/.files/vimrc .vimrc
+ln -s ~/.files/vim .vim
 vim +qa
 vim +PlugInstall < /dev/tty
 
-sdd install oh-my-zsh tmux direnv symlinks fzf keyboard
+sdd install oh-my-zsh tmux pip direnv symlinks fzf keyboard st
+sdd install watson=aa901567c5aa6129ff6dae799eddbfb0be06cb65
+
 echo "Set up .files/global_gituser!"
