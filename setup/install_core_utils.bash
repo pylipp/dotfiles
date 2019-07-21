bdb6e275d3ec6ad295dd7d8cba22fd2b3c874600
pylipp <philipp.metzner@tum.de>
2019-07-21 18:57:06 +0200

    Stop using usbmount package
    Promising replacement: https://github.com/pylipp/automount-usb

--- setup/install_core_utils.bash
+++ setup/install_core_utils.bash
@@ -20,19 +20,13 @@ main() {
         # workflow
         install_packages tig silversearcher-ag xclip
         # system utilities
-        install_packages network-manager usbmount lm-sensors htop
+        install_packages network-manager lm-sensors htop
         # libraries
         install_packages libxml2-dev libxslt-dev libxml2-utils libxcb-composite0-dev
         # sound support
         install_packages pulseaudio libasound2-dev alsa-utils
         # base utilities
         install_packages wget curl gawk scrot zip unzip 
-
-        # make automounted devices readable for user
-        # https://unix.stackexchange.com/a/155689/192726
-        if [[ -e /etc/usbmount/usbmount.conf ]]; then
-            sudo sed -i -r 's/(^MOUNTOPTIONS=".*)"/\1,uid=1000,gid=1000"/' /etc/usbmount/usbmount.conf
-        fi
 
         # make zathura default application to open pdfs using xdg-open
         xdg-mime default zathura.desktop application/pdf
