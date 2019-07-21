b514dc75a7c8498dc73fa24bad1e93d419093e55
pylipp <philipp.metzner@tum.de>
2019-07-21 19:00:11 +0200

    Install dunst from deb repo instead from source
    v1.1.0 in stretch was quite outdated but buster brings v1.3.2.

--- setup/debian/setup_i3.bash
+++ setup/debian/setup_i3.bash
@@ -17,19 +17,7 @@ main() {
     if [[ $method = "global" ]]; then
         echo_install_info i3
 
-        install_packages git build-essential
-        install_packages i3 i3lock xautolock xorg xinit feh scrot imagemagick arandr
-
-        mkcswdir
-
-        # install dunst from source. v1.1.0 from the debian repos is outdated
-        install_packages libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev \
-            libglib2.0-dev libpango1.0-dev libgtk-3-dev libxdg-basedir-dev
-        git clone https://github.com/dunst-project/dunst
-        cd dunst
-        git checkout v1.2.0
-        make
-        sudo make install
+        install_packages i3 i3lock xautolock xorg xinit feh scrot imagemagick arandr dunst
     elif [[ $method = "local" ]]; then
         echo_warn "Sorry, local i3 installation is not possible."
     else
