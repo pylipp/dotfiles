b52572d65a7d8c8b61c6582a3b3ef70313a13fe0
pylipp <philipp.metzner@tum.de>
2019-07-21 19:14:14 +0200

    Simplify install_core_utils script
    - Remove unnecessary packages (replacements exist; e.g. rg for ag,
    u-ctags for e-ctags)

--- setup/install_core_utils.bash
+++ setup/install_core_utils.bash
@@ -1,42 +1,24 @@
 #! /bin/bash
 
 # Installation script for core tools
-# Installation location: /usr and ~/
-# Requires: - git
 
 set -e
 
-source $(dirname "$0")/utils.bash
+install_packages() {
+    sudo apt-get install --yes "$@"
+}
 
 main() {
-    local method=${1:-global}
-
-    if [[ $method = "global" ]]; then
-        echo_install_info "core utils"
-
-        # development utilities
-        install_packages \
-            cmake build-essential doxygen graphviz python-dev exuberant-ctags
-        # workflow
-        install_packages tig silversearcher-ag
-        # system utilities
-        install_packages network-manager lm-sensors htop
-        # libraries
-        install_packages libxml2-dev libxslt-dev libxml2-utils libxcb-composite0-dev
-        # sound support
-        install_packages pulseaudio libasound2-dev alsa-utils
-        # base utilities
-        install_packages wget curl gawk zip unzip
-
-        # make zathura default application to open pdfs using xdg-open
-        xdg-mime default zathura.desktop application/pdf
-
-    elif [[ $method = "local" ]]; then
-        echo_warn "Sorry, local core utils installation is not possible."
-    else
-        echo_error "Unknown method '$method'!"
-        exit 1
-    fi
+    # development utilities
+    install_packages cmake build-essential
+    # workflow
+    install_packages tig
+    # system utilities
+    install_packages network-manager lm-sensors htop
+    # sound support
+    install_packages pulseaudio libasound2-dev alsa-utils
+    # base utilities
+    install_packages curl gawk zip unzip
 }
 
 main "$@"
