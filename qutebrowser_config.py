171724012012344835aefb254abbb7096bda6713
pylipp <philipp.metzner@tum.de>
2020-06-04 17:52:26 +0200

    Update qutebrowser_config

--- qutebrowser_config.py
+++ qutebrowser_config.py
@@ -42,9 +42,14 @@ config.set('content.javascript.enabled', True, 'qute://*/*')
 # Type: ShellCommand
 c.editor.command = ['st', '-e', '/usr/bin/vim', '{}']
 
-# Background color of selected even tabs.
-# Type: QtColor
-c.colors.tabs.selected.even.bg = 'darkorange'
+# Automatically enter insert mode if an editable element is focused
+# after loading the page.
+# Type: Bool
+c.input.insert_mode.auto_load = True
+
+# Padding (in pixels) around text for tabs.
+# Type: Padding
+c.tabs.padding = {'bottom': 2, 'right': 5, 'top': 2, 'left': 5}
 
 # Position of the tab bar.
 # Type: Position
@@ -55,38 +60,18 @@ c.colors.tabs.selected.even.bg = 'darkorange'
 #   - right
 c.tabs.position = 'bottom'
 
-# Automatically enter insert mode if an editable element is focused
-# after loading the page.
-# Type: Bool
-c.input.insert_mode.auto_load = True
-
 # Background color of selected odd tabs.
 # Type: QtColor
 c.colors.tabs.selected.odd.bg = 'darkorange'
 
-# Padding (in pixels) around text for tabs.
-# Type: Padding
-c.tabs.padding = {'bottom': 2, 'right': 5, 'top': 2, 'left': 5}
+# Background color of selected even tabs.
+# Type: QtColor
+c.colors.tabs.selected.even.bg = 'darkorange'
 
 # Default monospace fonts. Whenever "monospace" is used in a font
 # setting, it's replaced with the fonts listed here.
 # Type: Font
 c.fonts.monospace = 'Inconsolata'
-
-# Allow JavaScript to read from or write to the clipboard. With
-# QtWebEngine, writing the clipboard as response to a user interaction
-# is always allowed.
-# Type: Bool
-c.content.javascript.can_access_clipboard = True
-
-# Always restore open sites when qutebrowser is reopened.
-# Type: Bool
-c.auto_save.session = True
-
-# User agent to send. Unset to send the default. Note that the value
-# read from JavaScript is always the global value.
-# Type: String
-config.set('content.headers.user_agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0', 'https://accounts.google.com/*')
 
 # Bindings for normal mode
 config.bind('<Ctrl+\\>', 'tab-focus last')
