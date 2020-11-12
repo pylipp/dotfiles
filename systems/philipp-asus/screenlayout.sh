61a2dc094a1c3a2454f7e3e94be49da8b2a4a620
pylipp <philipp.metzner@tum.de>
2020-11-12 19:30:13 +0100

    Adjust brightness of laptop screen

--- systems/philipp-asus/screenlayout.sh
+++ systems/philipp-asus/screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 1920x312 --rotate normal --output HDMI-2 --off
+xrandr --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --primary --brightness 0.8 --mode 1366x768 --pos 1920x312 --rotate normal --output HDMI-2 --off
