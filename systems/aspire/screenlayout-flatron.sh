f609d9718e0a617ed560b68f2202c535f2b7f2df
pylipp <beth.aleph@yahoo.de>
2024-05-09 15:10:02 +0200

    Update screenlayout

---
+++ systems/aspire/screenlayout-flatron.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-1 --primary --mode 1366x768 --pos 1680x282 --rotate normal --output HDMI-1 --off --output VGA-1 --mode 1680x1050 --pos 0x0 --rotate normal
--- systems/aspire/screenlayout.sh
+++ systems/aspire/screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1366x768 --pos 1680x282 --rotate normal --output HDMI-1 --off --output VGA-1 --mode 1680x1050 --pos 0x0 --rotate normal
+xrandr --output LVDS-1 --primary --mode 1366x768 --pos 0x312 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output VGA-1 --off
