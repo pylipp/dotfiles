2e7b424205479206eb789a4d37106e6c4ba485b9
pylipp <beth.aleph@yahoo.de>
2025-01-21 22:02:50 +0100

    Update aspire screenlayout

--- systems/aspire/screenlayout.sh
+++ systems/aspire/screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1366x768 --pos 0x312 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output VGA-1 --off
+xrandr --output LVDS-1 --primary --mode 1366x768 --pos 1920x312 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output VGA-1 --off
